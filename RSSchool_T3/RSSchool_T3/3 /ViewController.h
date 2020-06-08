485611369a890fe5b475c1dea0c648cf8b7b604a
Lina Loyko <linaloyko@icloud.com>
2020-06-08 19:22:48 +0300

    Update solution for exercise 3

--- RSSchool_T3/RSSchool_T3/3 /ViewController.h
+++ RSSchool_T3/RSSchool_T3/3 /ViewController.h
@@ -1,6 +1,6 @@
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController <UITextFieldDelegate>
+@interface ViewController : UIViewController
 
 @property (nonatomic, strong) UITextField *textFieldRed;
 @property (nonatomic, strong) UITextField *textFieldGreen;
@@ -12,12 +12,22 @@
 @property (nonatomic, strong) UIView *viewResultColor;
 @property (nonatomic, strong) UIButton *buttonProcess;
 
+- (void)setUIElements;
+- (void)setConstraits;
+- (void)setConstraintsForTextField:(UITextField *)textField topAnchor:(NSLayoutAnchor *)topAnchor;
+- (void)setUpTextField:(UITextField *)textfield withIdentifier:(NSString *)identifier;
+- (void)setUpLabel:(UILabel *)label withIdentifier:(NSString *)identifier andText:(NSString *)text;
 - (void)calculateColor;
 - (NSString*)hexColor:(UIColor*)color;
 - (BOOL)isInteger:(NSString*)str;
+- (BOOL)isValidColor;
+
+@end
+
+@interface ViewController(TextFieldDelegate) <UITextFieldDelegate>
+
 - (void)textFieldDidBeginEditing:(UITextField *)textField;
 - (BOOL)textFieldShouldReturn:(UITextField *)textField;
-- (BOOL)isValidColor;
 
 @end
 
--- RSSchool_T3/RSSchool_T3/3 /ViewController.m
+++ RSSchool_T3/RSSchool_T3/3 /ViewController.m
@@ -1,66 +1,102 @@
 #import "ViewController.h"
 
+static const CGFloat kSpacing = 40;
+static const CGFloat kLeadingLabels = 25;
+static const CGFloat kLeadingTextFields = 100;
+static const CGFloat kTrailing = -25;
+static const CGFloat kLeadingViewResult = 130;
+static const CGFloat kTopLabel = 40;
+static const CGFloat kHeight = 40;
+static const CGFloat kWidth = 60;
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setUIElements];
+    [self setConstraits];
 }
 
 - (void)setUIElements {
     self.view.accessibilityIdentifier = @"mainView";
   
-    self.textFieldRed = [[UITextField alloc] initWithFrame:CGRectMake(100.0, 200.0, 270.0, 40.0)];
-    self.textFieldRed.accessibilityIdentifier = @"textFieldRed";
-    self.textFieldRed.placeholder = @"0..255";
-    self.textFieldRed.borderStyle = UITextBorderStyleRoundedRect;
-    self.textFieldRed.delegate = self;
-    [self.view addSubview:self.textFieldRed];
+    self.textFieldRed = [[UITextField alloc] init];
+    [self setUpTextField:self.textFieldRed withIdentifier:@"textFieldRed"];
     
-    self.textFieldGreen = [[UITextField alloc] initWithFrame:CGRectMake(100.0, 280.0, 270.0, 40.0)];
-    self.textFieldGreen.accessibilityIdentifier = @"textFieldGreen";
-    self.textFieldGreen.placeholder = @"0..255";
-    self.textFieldGreen.borderStyle = UITextBorderStyleRoundedRect;
-    self.textFieldGreen.delegate = self;
-    [self.view addSubview:self.textFieldGreen];
+    self.textFieldGreen = [[UITextField alloc] init];
+    [self setUpTextField:self.textFieldGreen withIdentifier:@"textFieldGreen"];
     
-    self.textFieldBlue = [[UITextField alloc] initWithFrame:CGRectMake(100.0, 360.0, 270.0, 40.0)];
-    self.textFieldBlue.accessibilityIdentifier = @"textFieldBlue";
-    self.textFieldBlue.placeholder = @"0..255";
-    self.textFieldBlue.borderStyle = UITextBorderStyleRoundedRect;
-    self.textFieldBlue.delegate = self;
-    [self.view addSubview:self.textFieldBlue];
+    self.textFieldBlue = [[UITextField alloc] init];
+    [self setUpTextField:self.textFieldBlue withIdentifier:@"textFieldBlue"];
     
-    self.labelRed = [[UILabel alloc] initWithFrame:CGRectMake(25, 200, 60, 40)];
-    self.labelRed.accessibilityIdentifier = @"labelRed";
-    self.labelRed.text = @"RED";
-    [self.view addSubview:self.labelRed];
+    self.labelResultColor = [[UILabel alloc] initWithFrame:CGRectMake(kLeadingLabels, kTopLabel, kWidth, kHeight)];
+    [self setUpLabel:self.labelResultColor withIdentifier:@"labelResultColor" andText:@"Color"];
     
-    self.labelGreen = [[UILabel alloc] initWithFrame:CGRectMake(25, 280, 60, 40)];
-    self.labelGreen.accessibilityIdentifier = @"labelGreen";
-    self.labelGreen.text = @"GREEN";
-    [self.view addSubview:self.labelGreen];
+    self.labelRed = [[UILabel alloc] initWithFrame:CGRectMake(kLeadingLabels, self.labelResultColor.frame.origin.y + kHeight + kSpacing,
+                                                              kWidth, kHeight)];
+    [self setUpLabel:self.labelRed withIdentifier:@"labelRed" andText:@"RED"];
+
+    self.labelGreen = [[UILabel alloc] initWithFrame:CGRectMake(kLeadingLabels, self.labelRed.frame.origin.y + kHeight + kSpacing,
+                                                                kWidth, kHeight)];
+    [self setUpLabel:self.labelGreen withIdentifier:@"labelGreen" andText:@"GREEN"];
+
+    self.labelBlue = [[UILabel alloc] initWithFrame:CGRectMake(kLeadingLabels, self.labelGreen.frame.origin.y + kHeight + kSpacing,
+                                                               kWidth, kHeight)];
+    [self setUpLabel:self.labelBlue withIdentifier:@"labelBlue" andText:@"BLUE"];
     
-    self.labelBlue = [[UILabel alloc] initWithFrame:CGRectMake(25, 360, 60, 40)];
-    self.labelBlue.accessibilityIdentifier = @"labelBlue";
-    self.labelBlue.text = @"BLUE";
-    [self.view addSubview:self.labelBlue];
-    
-    self.labelResultColor = [[UILabel alloc] initWithFrame:CGRectMake(25, 120, 90, 40)];
-    self.labelResultColor.accessibilityIdentifier = @"labelResultColor";
-    self.labelResultColor.text = @"Color";
-    [self.view addSubview:self.labelResultColor];
-    
-    self.viewResultColor = [[UIView alloc] initWithFrame:CGRectMake(120, 120, 250.0, 40.0)];
+    self.viewResultColor = [[UIView alloc] init];
     self.viewResultColor.accessibilityIdentifier = @"viewResultColor";
     [self.view addSubview:self.viewResultColor];
     
-    UIButton *buttonProcess = [UIButton buttonWithType:UIButtonTypeSystem];
-    buttonProcess.accessibilityIdentifier = @"buttonProcess";
-    [buttonProcess setTitle:@"Process" forState: UIControlStateNormal];
-    buttonProcess.frame = CGRectMake(177, 440, 60, 40);
-    [buttonProcess addTarget:self action:@selector(calculateColor) forControlEvents:UIControlEventTouchUpInside];
-    [self.view addSubview:buttonProcess];
+    self.buttonProcess = [UIButton buttonWithType:UIButtonTypeSystem];
+    self.buttonProcess.accessibilityIdentifier = @"buttonProcess";
+    [self.buttonProcess setTitle:@"Process" forState: UIControlStateNormal];
+    [self.buttonProcess addTarget:self action:@selector(calculateColor) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.buttonProcess];
+}
+
+- (void)setConstraits {
+    [self setConstraintsForTextField:self.textFieldRed topAnchor:self.viewResultColor.bottomAnchor];
+    [self setConstraintsForTextField:self.textFieldGreen topAnchor:self.textFieldRed.bottomAnchor];
+    [self setConstraintsForTextField:self.textFieldBlue topAnchor:self.textFieldGreen.bottomAnchor];
+    
+    self.viewResultColor.translatesAutoresizingMaskIntoConstraints = NO;
+    self.buttonProcess.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [NSLayoutConstraint activateConstraints:@[
+        [self.viewResultColor.heightAnchor constraintEqualToConstant:kHeight],
+        [self.viewResultColor.topAnchor constraintEqualToAnchor:self.view.topAnchor constant:kSpacing],
+        [self.viewResultColor.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:kLeadingViewResult],
+        [self.viewResultColor.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:kTrailing],
+    
+        [self.buttonProcess.topAnchor constraintEqualToAnchor:self.labelBlue.bottomAnchor constant:kSpacing],
+        [self.buttonProcess.centerXAnchor constraintEqualToAnchor:self.view.centerXAnchor]
+    ]];
+}
+
+- (void)setConstraintsForTextField:(UITextField *)textField topAnchor:(NSLayoutAnchor *)topAnchor {
+    textField.translatesAutoresizingMaskIntoConstraints = NO;
+    [NSLayoutConstraint activateConstraints:@[
+           [textField.heightAnchor constraintEqualToConstant:kHeight],
+           [textField.topAnchor constraintEqualToAnchor:topAnchor constant:kSpacing],
+           [textField.leadingAnchor constraintEqualToAnchor:self.view.leadingAnchor constant:kLeadingTextFields],
+           [textField.trailingAnchor constraintEqualToAnchor:self.view.trailingAnchor constant:kTrailing],
+    ]];
+}
+
+- (void)setUpTextField:(UITextField *)textfield withIdentifier:(NSString *)identifier {
+    textfield.accessibilityIdentifier = identifier;
+    textfield.placeholder = @"0..255";
+    textfield.borderStyle = UITextBorderStyleRoundedRect;
+    textfield.delegate = self;
+    textfield.allowsEditingTextAttributes = true;
+    [self.view addSubview:textfield];
+}
+    
+- (void)setUpLabel:(UILabel *)label withIdentifier:(NSString *)identifier andText:(NSString *)text {
+    label.accessibilityIdentifier = identifier;
+    label.text = text;
+    [self.view addSubview:label];
 }
 
 - (void)calculateColor {
@@ -87,16 +123,6 @@
     return hexString;
 }
 
-- (void)textFieldDidBeginEditing:(UITextField *)textField {
-    self.labelResultColor.text = @"Color";
-    self.viewResultColor.backgroundColor = UIColor.whiteColor;
-}
-
-- (BOOL)textFieldShouldReturn:(UITextField *)textField {
-    [textField resignFirstResponder];
-    return YES;
-}
-
 - (BOOL)isValidColor {
     if ([self isInteger:self.textFieldRed.text] && [self isInteger:self.textFieldGreen.text] && [self isInteger:self.textFieldBlue.text]) {
         int r = [self.textFieldRed.text intValue];
@@ -116,6 +142,19 @@
 }
 
 @end
-    
+   
+@implementation ViewController (TextFieldDelegate)
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    self.labelResultColor.text = @"Color";
+    self.viewResultColor.backgroundColor = UIColor.clearColor;
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
+
+@end
 
 
